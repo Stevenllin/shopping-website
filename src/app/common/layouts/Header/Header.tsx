d6fe22687e5ef134109cf474adcc5e322c8a4008
Steven <stevenlin3263@gmail.com>
2023-07-03 17:21:53 +0800

    feat: Navbar 的 UI 完成

--- src/app/common/layouts/Header/Header.tsx
+++ src/app/common/layouts/Header/Header.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { PiShoppingCartSimpleBold } from 'react-icons/pi'
+import { BsFillPersonFill } from 'react-icons/bs'
 
 const Header: React.FC = () => {
+  const [menuBarActive, setMenuBarActive] = useState<boolean>(false);
+  console.log('menuBarActive', menuBarActive);
+
+  /**
+   * @description 處理視窗滾動控制 Menu 顯示/隱藏
+   */
+   useEffect(() => {
+    window.addEventListener('scroll', () => {
+      const indexShowcaseSectionMenu = document.getElementsByClassName('menu')[0];
+      if (indexShowcaseSectionMenu) {
+        const active = indexShowcaseSectionMenu.getBoundingClientRect().top < 0;
+        setMenuBarActive(active);
+      } else {
+        setMenuBarActive(false);
+      }
+    });
+  }, []);
+
   return (
     <header>
-      <nav>
-        <img src={require('../../../../assets/img/logo.png')} className='logo' alt='logo' />
+      <nav className="navbar fixed-top">
+        <div className="context">
+          <img src={require('../../../../assets/img/logo.png')} className='logo' alt='logo' />
+          <div className="d-flex">
+            <button type="button" className="me-3">
+              <BsFillPersonFill className="icons" />
+            </button>
+            <button type="button">
+              <PiShoppingCartSimpleBold className="icons" />
+            </button>
+          </div>
+        </div>
       </nav>
     </header>
   )
